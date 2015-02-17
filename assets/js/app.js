8f0fe552bfd19f0ea742e4f702160fc94542ed37
Nikolas Liolios <Nliolios@exeter.edu>
2015-02-17 14:22:36 -0500

    changed root

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var root = 'http://localhost:3000';
+var root = 'dineexeter.herokuapp.com';
 
 function currentMeal(fromMeal) {
 	var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
